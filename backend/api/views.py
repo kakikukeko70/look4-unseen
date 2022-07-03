715a24c66f0201b1aecd818329c417d83e32bac4
kakikukeko70 <tsusu3292@gmail.com>
2022-07-03 12:08:18 +0900

    urlを処理するモジュールに渡し，結果をresponseに渡す

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,17 @@
-from urllib import response
+from pprint import pprint
+
 from rest_framework import views, status
 from rest_framework.response import Response
 
-from .serializer import ChannelUrlSerializer 
+from .serializer import ChannelUrlSerializer
+from .random_videos import main
 
 class ChannelUrlApiView(views.APIView):
 
     def post(self, request):
+        print(request.data)
         serializer = ChannelUrlSerializer(data=request.data)
         serializer.is_valid()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        url = serializer.data
+        videos = main(url['url'])
+        return Response(videos, status.HTTP_201_CREATED)
